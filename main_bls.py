115c0b74a20ceea11adb29e7c0ab9987fde32ca0
GDS Solutions <sguri003@outlook.com>
2025-06-24 02:04:55 +0000

    CHANGE CLASSE

--- main_bls.py
+++ main_bls.py
@@ -8,7 +8,7 @@
 import csv
 import numpy as np
 import pandas as pd
-from CPI_Puller import CPI_Puller
+import CPI_Puller
 
 # See the code in c_bls_data.py for descriptions of the class's input parameters.
 # Create and instance of c_bls_data to retrieve cpi data for 1) all items and 2)
@@ -24,4 +24,4 @@ BLS_API_KEY = df_ky['API'][0]
 #OUTPUT DEFLATOR ID: IPUCN2211__T051000000, REAL SECTOR OUTPUT ID: IPUCN2211__T011000000
 #bls_dt = labor_US(BLS_API_KEY, 'POWER DELIVER SG.CSV' , ['IPUCN2211__T051000000', 'IPUCN2211__T011000000'], 2000, 2022 )
 #CPI for Gas, Groceries, necessities. 
-bls_dt = CPI_Puller(BLS_API_KEY, 'CPI_2025_06_22.csv',['CUSR0000SA0', 'CUSR0000SETB01', 'CUSR0000SAF1', 'CUSR0000SETA02', ], 2019, 2025)
+bls_dt = CPI_Puller(BLS_API_KEY, 'CPI_2025_06_22.csv',['CUSR0000SA0', 'CUSR0000SETB01', 'CUSR0000SAF1', 'CUSR0000SETA02'], 2019, 2025)
